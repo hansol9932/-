567430418fae76bae90db7cabdfab99fb4dc1e40
hansol9932 <ssooll9932@gmail.com>
2024-05-16 17:51:07 +0900

    헤더 스크롤 시 로고 부분 green 으로 교체 후, footer 패딩과 마진 조정// 서브페이지 footer id 명 수정

--- script/sub.js
+++ script/sub.js
@@ -18,7 +18,9 @@ $(window).scroll(function () {
 	// console.log($(document).scrollTop());
 	if ($(document).scrollTop() > offset.top) {
 		$('#header_top').addClass('on');
+		$('#header_top img').attr('src', './img/logo(green).png');
 	} else {
 		$('#header_top').removeClass('on');
+		$('#header_top img').attr('src', './img/logo(w).png');
 	}
 });
