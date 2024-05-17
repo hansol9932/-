cfc0edfb499ed79fe85df329b5b0251cd8523272
hansol9932 <ssooll9932@gmail.com>
2024-05-17 09:06:47 +0900

    서브페이지 q&a 부분 뼈대잡고 탭메뉴 js 진행 // 글꼴 색상 등 디자인적 css 수정필요

--- script/sub.js
+++ script/sub.js
@@ -1,4 +1,4 @@
-console.log('제품콘솔');
+// sub_product 탭메뉴 부분
 
 $('.tab .sub_product_visual_inner ul li').click(function (event) {
 	event.preventDefault();
@@ -7,6 +7,17 @@ $('.tab .sub_product_visual_inner ul li').click(function (event) {
 
 	$(this).addClass('active');
 	$('#' + $(this).data('tab')).addClass('active');
+});
+
+// sub_FAQ 탭메뉴 부분
+
+$('.tabmenu .sub_FAQ_visual_inner ul li').click(function (event) {
+	event.preventDefault();
+	$('.tabmenu ul li').removeClass('on');
+	$('.tabmenu .wrap').removeClass('on');
+
+	$(this).addClass('on');
+	$('#' + $(this).data('tab')).addClass('on');
 });
 
 // 헤더 js 부분
@@ -18,9 +29,9 @@ $(window).scroll(function () {
 	// console.log($(document).scrollTop());
 	if ($(document).scrollTop() > offset.top) {
 		$('#header_top').addClass('on');
-		$('#header_top img').attr('src', './img/logo(green).png');
+		$('#header_top img').attr('src', '../img/logo(green).png');
 	} else {
 		$('#header_top').removeClass('on');
-		$('#header_top img').attr('src', './img/logo(w).png');
+		$('#header_top img').attr('src', '../img/logo(w).png');
 	}
 });
